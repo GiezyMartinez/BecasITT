5292b4d92db582a9727a93f684a59d9df445b25c
GiezyMartinez <48788928+GiezyMartinez@users.noreply.github.com>
2020-06-02 01:52:39 -0700

    generación de base de datos

--- BecasITT/Controllers/HomeController.cs
+++ BecasITT/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BecasITT.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace BecasITT.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult MostrarAlumno()
+        { return View(); }
         public ActionResult Index()
         {
             return View();
